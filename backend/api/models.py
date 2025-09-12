71c026fd32b31a7171ebc03696c304ac3a819275
Ekta Mehra <ekta.mehra@unifyenergy.co.uk>
2025-09-12 10:19:05 +0000

    add organisation class in models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,2 +1,12 @@
-# placeholder for future DB models
 from django.db import models
+
+# --- Core / Tenancy ---
+class Organization(models.Model):
+    """
+    Organisation in this case is Bruntwood.
+    It lets you segregate data by org and keeps names unique within each org.
+    """
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
