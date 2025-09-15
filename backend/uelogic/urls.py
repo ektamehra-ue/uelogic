2dc72014c7bf62b811f1f123454bbc73005ed894
Ekta Mehra <ekta.mehra@unifyenergy.co.uk>
2025-09-15 10:51:22 +0000

    did smoke test and found a redundant folder for now and deleted it, edited ../uelogic/urls.py file to match auth

--- backend/uelogic/urls.py
+++ backend/uelogic/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("api/v1/auth/jwt/create", TokenObtainPairView.as_view(), name="jwt-create"),
-    path("api/v1/auth/jwt/refresh", TokenRefreshView.as_view(), name="jwt-refresh"),
+    path("api/auth/jwt/create", TokenObtainPairView.as_view(), name="jwt-create"),
+    path("api/auth/jwt/refresh", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("admin/", admin.site.urls),
-    path("api/v1/", include("api.urls")),  # your DRF routes
+    path("api/", include("api.urls")),  # your DRF routes
+    path("api-auth/", include("rest_framework.urls"))  # enables Login in browsable API
 ]
